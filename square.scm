f86486abc9ec5d8d04d653ec16cbe45a2b8e48d4
Marek Polacek <polacek@redhat.com>
2021-01-09 14:13:54 -0500

    square: add comments.

--- square.scm
+++ square.scm
@@ -1,3 +1,5 @@
+;; SICP 1.1.4
+
 (define (square x) (* x x))
 
 (define (sum-of-squares x y)
@@ -6,4 +8,5 @@
 (define (f a)
  (sum-of-squares (+ a 1) (* a 2)))
 
+;; Print the result.
 (display (f 5)) (newline)
